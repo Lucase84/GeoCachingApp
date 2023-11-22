6877f74f2b8d0980e03aca7d99dab8b484e715b1
Lucase84 <lucas.dudot@epitech.eu>
2023-11-22 16:55:14 +0100

    Add a slide animation to found button

--- lib/Views/geo_cache_details.dart
+++ lib/Views/geo_cache_details.dart
@@ -1,10 +1,39 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Objects/geo_cache_marker.dart';
 
-class GeoCacheDetails extends StatelessWidget {
+class GeoCacheDetails extends StatefulWidget {
   const GeoCacheDetails({required this.geoCacheMarker, super.key});
 
   final GeoCacheMarker geoCacheMarker;
+
+  @override
+  State<GeoCacheDetails> createState() => _GeoCacheDetailsState();
+}
+
+class _GeoCacheDetailsState extends State<GeoCacheDetails>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<int> _animation;
+  final int _maxWidth = 200;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _animation =
+        IntTween(begin: 0, end: _maxWidth).animate(_animationController);
+    _animation.addListener(
+      () => setState(() {
+        if (_animationController.value == 1.0) {
+          Navigator.pop(context);
+        }
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +44,7 @@ class GeoCacheDetails extends StatelessWidget {
           color: Theme.of(context).colorScheme.onPrimary,
         ),
         title: Text(
-          geoCacheMarker.name,
+          widget.geoCacheMarker.name,
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -29,7 +58,7 @@ class GeoCacheDetails extends StatelessWidget {
           const SizedBox(height: 20),
           Center(
             child: Image.network(
-              geoCacheMarker.photo,
+              widget.geoCacheMarker.photo,
               width: MediaQuery.of(context).size.width * 0.95,
               height: MediaQuery.of(context).size.height * 0.3,
               fit: BoxFit.cover,
@@ -37,7 +66,7 @@ class GeoCacheDetails extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           Text(
-            'Coordinates: ${geoCacheMarker.position.latitude}, ${geoCacheMarker.position.longitude}',
+            'Coordinates: ${widget.geoCacheMarker.position.latitude}, ${widget.geoCacheMarker.position.longitude}',
             style: TextStyle(
               fontSize: 20,
               fontWeight: FontWeight.bold,
@@ -46,24 +75,54 @@ class GeoCacheDetails extends StatelessWidget {
           ),
           const SizedBox(height: 200),
           GestureDetector(
-            onTap: () {},
-            child: Container(
-              width: MediaQuery.of(context).size.width * 0.4,
-              height: 50,
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: const Center(
-                child: Text(
-                  'Found !',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+            onLongPressCancel: () {
+              _animationController.reverse();
+            },
+            onLongPressDown: (LongPressDownDetails details) {
+              if (_animationController.value == 0.0) {
+                _animationController.forward();
+              } else {
+                _animationController.reverse();
+              }
+            },
+            onLongPressEnd: (LongPressEndDetails details) {
+              if (_animationController.value < 1) {
+                _animationController.reverse();
+              } else {
+                _animationController.reset();
+              }
+            },
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  width: _maxWidth.toDouble(),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Found !',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                Container(
+                  width: _animation.value.toDouble(),
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(
+                      Theme.of(context).colorScheme.error.value & 0x44FFFFFF,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
