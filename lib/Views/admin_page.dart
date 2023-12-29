811a0e81f360f2fc6ac9659de5b865e547c44dda
Lucase84 <lucas.dudot@epitech.eu>
2023-12-29 16:53:53 +0100

    Implement the new pages to the admin view

--- lib/Views/admin_page.dart
+++ lib/Views/admin_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Routes/authentification.dart';
+import 'package:flutter_application_1/Views/admin/geo_caches_management.dart';
+import 'package:flutter_application_1/Views/admin/users_management.dart';
+import 'package:flutter_application_1/Views/login_page.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/State_manager/user_manager.dart';
 
 class AdminPage extends StatefulWidget {
   const AdminPage({super.key});
@@ -8,14 +14,56 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
+  int currentPage = 0;
+  final List<Widget> pages = const <Widget>[
+    GeoCachesManagement(),
+    UserManagement(),
+  ];
+
+  Future<void> _logOut() async {
+    context.read<UserModel>().setUser(null);
+    await AuthenticationService().signOut();
+    if (context.mounted) {
+      await Navigator.of(context).pushReplacement(
+        MaterialPageRoute<LoginPage>(
+          builder: (BuildContext context) => const LoginPage(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Admin Page'),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: _logOut,
+          ),
+        ],
       ),
-      body: const Center(
-        child: Text('Admin Page'),
+      body: pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: 'GeoCaches',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Users',
+          ),
+        ],
       ),
     );
   }
