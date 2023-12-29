75f2bf666566a16f1a095e194eb565ff642985d9
Lucase84 <lucas.dudot@epitech.eu>
2023-12-29 16:53:12 +0100

    Create user management view for admins

---
+++ lib/Views/admin/users_management.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UserManagement extends StatefulWidget {
+  const UserManagement({super.key});
+
+  @override
+  State<UserManagement> createState() => _UserManagementState();
+}
+
+class _UserManagementState extends State<UserManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Users gestion page'),
+      ),
+    );
+  }
+}
