b13eb3e6ee6d2fa040e897c909fa2d0e7420f22b
Lucase84 <lucas.dudot@epitech.eu>
2023-12-29 18:08:55 +0100

    Adding missing documentation

--- lib/Objects/geo_cache_marker.dart
+++ lib/Objects/geo_cache_marker.dart
@@ -1,6 +1,8 @@
 import 'package:latlong2/latlong.dart';
 
+/// GeoCacheMarker object
 class GeoCacheMarker {
+  /// Constructor for GeoCacheMarker object with required parameters
   GeoCacheMarker({
     required this.position,
     required this.photo,
@@ -8,8 +10,15 @@ class GeoCacheMarker {
     required this.id,
   });
 
+  /// Position of the marker on the map (latitude and longitude)
   LatLng position;
+
+  /// Photo of the marker (URL)
   String photo;
+
+  /// Name of the marker
   String name;
+
+  /// Id of the marker (used to identify the marker in the database)
   String id;
 }
