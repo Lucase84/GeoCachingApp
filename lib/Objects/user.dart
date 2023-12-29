2599a3bd7f90a259d1635a5f708abe46ae55de6f
Lucase84 <lucas.dudot@epitech.eu>
2023-12-29 18:10:07 +0100

    Adding missing documentation

--- lib/Objects/user.dart
+++ lib/Objects/user.dart
@@ -1,9 +1,14 @@
+/// This class is used to store the user data
 class UserData {
+  /// Constructor for UserData object with required parameters
   UserData({
     required this.email,
     required this.id,
   });
 
+  /// Email of the user
   String email;
+
+  /// Id of the user (used to identify the user in the database)
   String id;
 }
